696a84bef0724534ad7bdeda5c06625d1a914596
SHS <hunsuk218@gmail.com>
2017-11-13 14:28:54 +0900

    06

--- web_so.js
+++ web_so.js
@@ -1,7 +1,7 @@
 const http = require('http'), gpio = require('wiring-pi');
 const fs = require('fs');
 const socketio = require('socket.io');
-const LED = 29;
+const LEDRED = 29, LEDBLUE = 28, LEDGREEN = 25;
 const TRIG = 26, ECHO = 27;
 var startTime, travelTime;
 var index = 0, value = [];
@@ -16,10 +16,14 @@ const server = http.createServer(function(request, response) {
   });
 }).listen(65001, function() {
   gpio.wiringPiSetup();
-  gpio.pinMode(LED, gpio.OUTPUT);
+  gpio.pinMode(LEDBLUE, gpio.OUTPUT);
+  gpio.pinMode(LEDRED, gpio.OUTPUT);
+  gpio.pinMode(LEDGREEN, gpio.OUTPUT);
   gpio.pinMode(ECHO, gpio.INPUT);
   gpio.pinMode(TRIG, gpio.OUTPUT);
-  gpio.digitalWrite(LED, 0); // LED초기화
+  gpio.digitalWrite(LEDRED, 0); // LED초기화
+  gpio.digitalWrite(LEDBLUE, 0); // LED초기화
+  gpio.digitalWrite(LEDGREEN, 0); // LED초기화
   console.log('Server running at http://IP주소:65001');
 });
 const io = socketio.listen(server);
@@ -47,13 +51,28 @@ const watchon = () => {
   travelTime = gpio.micros() - startTime;
   distance = travelTime / 58;
   if (distance < 400) { // 센서는 400 cm 이내만 측정가능 함
+    if(distance <10){
+      gpio.digitalWrite(LEDGREEN,0);
+      gpio.digitalWrite(LEDBLUE,0);
+      gpio.digitalWrite(LEDRED,1);
+    }
+    else if(distance >=10 & distance<50){
+      gpio.digitalWrite(LEDGREEN,0);
+      gpio.digitalWrite(LEDBLUE,1);
+      gpio.digitalWrite(LEDRED,0);
+    }
+    else if(distance >= 50){
+      gpio.digitalWrite(LEDGREEN,1);
+      gpio.digitalWrite(LEDBLUE,0);
+      gpio.digitalWrite(LEDRED,0);
+    }
+
+
     if (index < 500) {
       value[index++] = distance;
       console.log('근접거리: %d cm', value[index - 1]);
       io.sockets.emit('watch', value[index - 1]);
     } else index = 0;
-    if ((index % 2) == 0) gpio.digitalWrite(LED, 1); // 가동중 LED점멸
-    else gpio.digitalWrite(LED, 0);
   }
   timerid = setTimeout(watchon, timeout);
 }
