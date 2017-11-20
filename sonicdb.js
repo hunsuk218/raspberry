1178fa69705c17e6437f139432bfc9bd1c8ed4f4
SHS <hunsuk218@gmail.com>
2017-11-20 14:50:15 +0900

    05

--- sonicdb.js
+++ sonicdb.js
@@ -9,8 +9,8 @@ var travelTime;
 const client = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'root',
-  password: 'gachon654321',
+  user: 'hooney',
+  password: 'hch',
   database: 'sensordb'
 });
 
